541e639ff6ebadd894a0c4236ad42580abe60c62
nareik90 <nareik90>
2016-04-10 19:26:22 +0100

    update

---
+++ Assets/Fx Explosion Pack/Script/DestroyEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEffect : MonoBehaviour {
+
+    void OnTriggerEnter(Collider co)
+    {
+        // If target then deal Damage, destroy self
+        if (co.name == "Ground")
+        { 
+            Destroy(gameObject);
+            Debug.Log("Target hit... Bang");
+        }
+    }
+}
---
+++ Assets/Fx Explosion Pack/Script/explosion.js
@@ -0,0 +1,18 @@
+﻿var EffectName = ["Explosion1", "Explosion2", "Explosion3", "Explsoion4","Explosion5","Explosion6","Explosion7","Explosion8","Explosion9","Explosion10"];
+var Effect = new Transform[53];
+var i : int = 0;
+
+function Start()
+{
+     //Instantiate(Effect[i], Vector3(0,0,0),Quaternion.identity);
+}
+
+function OnCollisionEnter (col : Collision)
+    {
+        if(col.gameObject.name == "Ground")
+        {
+            Debug.Log("Bang...");
+            //Instantiate(Effect[i], Vector3(0,0,0),Quaternion.identity);
+            var obx = Instantiate(Effect[i], Vector3(0,0,0),Quaternion.identity);
+        }
+}
